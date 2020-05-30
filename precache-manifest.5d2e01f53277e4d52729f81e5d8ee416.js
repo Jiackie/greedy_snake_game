46216fbe92ae6c3bfc40c99d54d3c740a4acb795
Jiackie <Jiaqili1023@gmail.com>
2020-05-29 23:02:30 -0700

    Updates

---
+++ precache-manifest.5d2e01f53277e4d52729f81e5d8ee416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dc895f22591299178592b76998b514",
+    "url": "/greedy_snake_game/index.html"
+  },
+  {
+    "revision": "399e095fde854cc980b7",
+    "url": "/greedy_snake_game/static/css/main.84ca17a2.chunk.css"
+  },
+  {
+    "revision": "eb954e1bd14447d5b2dc",
+    "url": "/greedy_snake_game/static/js/2.55414915.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greedy_snake_game/static/js/2.55414915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399e095fde854cc980b7",
+    "url": "/greedy_snake_game/static/js/main.3ae6775d.chunk.js"
+  },
+  {
+    "revision": "c5db6a03501f84447d72",
+    "url": "/greedy_snake_game/static/js/runtime-main.de42eea7.js"
+  }
+]);
